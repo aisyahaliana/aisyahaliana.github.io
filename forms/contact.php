faa9a157cc2f00e141963b016b202ee6f0d93806
aisyahaliana <chickendinnergo@gmail.com>
2020-11-01 20:03:44 -0800

    okey

--- forms/contact.php
+++ forms/contact.php
@@ -1,26 +1,24 @@
 <?php
+   
+if(isset($_POST['submit'])){
+
    $name = $_POST['name'];
-   $visitor_email = $_POST['email'];
-   $subject = $_POST['subject'];
+   $email = $_POST['email'];
+   $messagesubject = $_POST['subject'];
    $message = $_POST['message'];
 
+   $to='aisyahaliana77@gmail.com';
+   $subject= 'New Form Submission';
+   $message= "Name: ".$name."\n".$email."\n". "Leave your comment: "."\n\n".$message;
 
-   $email_from = 'aisyahaliana@aisyahaliana.github.io';
-   $email_subject = "New form submission";
-   $email_body = "User Name: $name.\n".
-                   "User Email: $visitor_email.\n".
-                     "Subject: $subject.\n.
-                       "User Message: $message.\n";
-
-   $to = "aisyahaliana77@gmail.com";
-   $headers = "From: $email_from\r\n";
-   $headers .= "Reply-To: $visitor_email \r\n";
-
-   mail($to,$email_subject,$email_body,$headers);
-
-   header("Location: contact.html");
-
-
+   if(mail($to, $subject, $message, $headers)){
+    echo "<h2> Your message has been sent. Thank You!"." ".$name.";
+    }
+    else{
+        
+    echo "Something went wrong!";
+}
+}
 
 ?>
 
